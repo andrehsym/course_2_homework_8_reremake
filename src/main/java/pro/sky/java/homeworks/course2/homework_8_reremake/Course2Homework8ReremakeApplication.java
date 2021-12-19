7b90d78443d3d4f069bfaf6042ac60668ae1db9f
andrehsym <kochetkovandre1993@gmail.com>
2021-12-19 16:17:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/homeworks/course2/homework_8_reremake/Course2Homework8ReremakeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.homeworks.course2.homework_8_reremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Homework8ReremakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Homework8ReremakeApplication.class, args);
+    }
+
+}
